4ed3afc61b5ea9fe7cc4ceffec0442c554a90490
Pan Cheng <pchengeducation@hotmail.com>
2018-08-20 00:50:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class application
 
-  @@items = [Item.new{"Figs", 3.42), Item.new("Pears", 0.99)]
+  @@items = [Item.new("Figs", 3.42), Item.new("Pears", 0.99)]
 
   def call(env)
     resp = Rack::Response.new
